f4642f5df11324ae6f643a2699b83ff4c4304c4f
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2009-05-23 14:59:50 +0000

    jsVortex: * [fix] Added documentation to VortexFrame class

--- trunk/jsVortex/src/VortexFrame.js
+++ trunk/jsVortex/src/VortexFrame.js
@@ -4,10 +4,11 @@
  **/
 
 /**
- * @brief Creates a a new frame with the type configured
+ * BEEP frame instance. Creates a a new frame with the type configured
  * by the string received.
  *
  * @param strType The frame type to configure.
+ *
  */
 function VortexFrame (type,
 		      channel,
@@ -19,16 +20,60 @@ function VortexFrame (type,
 		      mimeHeaders,
 		      content) {
     /**
-     * @internal Property to hold frame type.
+     * Property to hold frame type. It is an string containing either
+     * "RPY", "MSG", "ERR", "SEQ", "ANS", "NUL".
      */
     this.type        = type;
+
+    /**
+     * Contains the channel number where the frame was received. It is a number.
+     */
     this.channel     = channel;
+
+    /**
+     * Contains the frame msgno identifier.
+     */
     this.msgno       = msgno;
+
+    /**
+     * Contains a boolean value representing if the frame has the more
+     * fragment enabled (pending content is to be received) or the
+     * frame represents a single and complete message sent by the
+     * remote BEEP peer.
+     */
     this.more        = more;
+
+    /**
+     * Contains the seqno value associated with the frame. Taking the
+     * exchange produced in a BEEP channel, this value contains the
+     * first byte contained in the frame, which belongs to that
+     * sequence.
+     */
     this.seqno       = seqno;
+    /**
+     * Contains the frame size notified. This may not be the same as
+     * the size of the content received since jsVortex does automatic
+     * MIME parsing. You can get the size of your message, without MIME
+     * headers, by calling to frame.content.length.
+     */
     this.size        = size;
+
+    /**
+     * Contains the ansno value, and identifier used inside one to
+     * many exchange style (ANS/NUL).
+     */
     this.ansno       = ansno;
+
+    /**
+     * Contains a list of mime headers. Each item is an instance of
+     * VortexMimeHeader.
+     */
     this.mimeHeaders = mimeHeaders;
+
+    /**
+     * Contains the content of the frame, without MIME headers which
+     * are processed and stored in mimeHeaders.
+     */
     this.content     = content;
 };
 
