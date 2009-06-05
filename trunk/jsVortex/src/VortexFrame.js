f1373571729fe71f12202b06e26ac980ca5d249b
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2009-06-05 22:14:18 +0000

    jsVortex: * [fix] More documentation..

--- trunk/jsVortex/src/VortexFrame.js
+++ trunk/jsVortex/src/VortexFrame.js
@@ -4,11 +4,32 @@
  **/
 
 /**
- * BEEP frame instance. Creates a a new frame with the type configured
- * by the string received.
+ * @brief BEEP frame instance. Creates a a new frame with the type
+ * configured by the string received.
  *
- * @param strType The frame type to configure.
+ * @param type {String} The frame type to configure.
  *
+ * @param channel {Number} Channel number where this frame was received.
+ *
+ * @param msgno {Number} Frame msgno value to configure.
+ *
+ * @param more {Boolean} Frame complete indicator.
+ *
+ * @param seqno {Number} Frame seqno value to configure.
+ *
+ * @param size {Number} Frame size value to configure.
+ *
+ * @param ansno {Number} ? Frame ansno value to configure. This value
+ * is optional if you aren't producing RPY, ERR or MSG frame.
+ *
+ * @param msgno {VortexMimeHeader []} Optional array of MIME headers
+ * to be configured as content received in the frame.
+ *
+ * @param content {String} The frame content.
+ *
+ * @class
+ *
+ * @return A newly \ref VortexFrame instance created.
  */
 function VortexFrame (type,
 		      channel,
@@ -20,59 +41,69 @@ function VortexFrame (type,
 		      mimeHeaders,
 		      content) {
     /**
-     * Property to hold frame type. It is an string containing either
-     * "RPY", "MSG", "ERR", "SEQ", "ANS", "NUL".
+     * @brief Property to hold frame type. It is an string containing
+     * either "RPY", "MSG", "ERR", "SEQ", "ANS", "NUL".
+     * @type {String}
      */
     this.type        = type;
 
     /**
-     * Contains the channel number where the frame was received. It is a number.
+     * @brief Contains the channel number where the frame was
+     * received. It is a number.
+     * @type {Number}
      */
     this.channel     = channel;
 
     /**
      * Contains the frame msgno identifier.
+     * @type {Number}
      */
     this.msgno       = msgno;
 
     /**
-     * Contains a boolean value representing if the frame has the more
-     * fragment enabled (pending content is to be received) or the
-     * frame represents a single and complete message sent by the
+     * @brief Contains a boolean value representing if the frame has
+     * the more fragment enabled (pending content is to be received) or
+     * the frame represents a single and complete message sent by the
      * remote BEEP peer.
+     * @type {Boolean}
      */
     this.more        = more;
 
     /**
-     * Contains the seqno value associated with the frame. Taking the
+     * @brief Contains the seqno value associated with the frame. Taking the
      * exchange produced in a BEEP channel, this value contains the
      * first byte contained in the frame, which belongs to that
      * sequence.
+     * @type {Number}
      */
     this.seqno       = seqno;
     /**
-     * Contains the frame size notified. This may not be the same as
+     * @brief Contains the frame size notified. This may not be the same as
      * the size of the content received since jsVortex does automatic
      * MIME parsing. You can get the size of your message, without MIME
      * headers, by calling to frame.content.length.
+     * @type {Number}
      */
     this.size        = size;
 
     /**
-     * Contains the ansno value, and identifier used inside one to
+     * @brief Contains the ansno value, and identifier used inside one to
      * many exchange style (ANS/NUL).
+     * @type {Number}
      */
     this.ansno       = ansno;
 
     /**
-     * Contains a list of mime headers. Each item is an instance of
-     * VortexMimeHeader.
+     * @brief Contains a list of mime headers. Each item is an instance of
+     * \ref VortexMimeHeader.
+     * @type {VortexMimeHeader []}
      */
     this.mimeHeaders = mimeHeaders;
 
     /**
-     * Contains the content of the frame, without MIME headers which
-     * are processed and stored in mimeHeaders.
+     * @brief Contains the content of the frame, without MIME headers
+     * which are processed and stored in mimeHeaders.
+     * @type {String}
      */
     this.content     = content;
 };
