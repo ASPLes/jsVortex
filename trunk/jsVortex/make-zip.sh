9d7e545eba4be3832c00df8fba3b627e7ff7cc07
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2009-06-26 14:00:38 +0000

    jsVortex: * [fix] Closing 0.2.1 release.

--- trunk/jsVortex/make-zip.sh
+++ trunk/jsVortex/make-zip.sh
@@ -5,5 +5,11 @@ jsvortex_version=`cat VERSION`
 
 # create temporal link to simulate release directory
 ln -s src jsvortex-${jsvortex_version}
-zip jsVortex-${jsvortex_version}.zip jsvortex-${jsvortex_version}/*.js; 
+ln -s ../doc/COPYING.txt src/COPYING.txt
+ln -s ../README src/README
+zip jsVortex-${jsvortex_version}.zip \
+    jsvortex-${jsvortex_version}/*.{js,txt} \
+    jsvortex-${jsvortex_version}/README
+rm src/README
+rm src/COPYING.txt
 rm jsvortex-${jsvortex_version}
