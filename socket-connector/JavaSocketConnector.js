b98a8e097422d188657800593ce5e24832b52f68
Francis Brosnan Blázquez <francis@aspl.es>
2009-11-24 13:02:31 +0000

    jsVortex: * [new] Added global variable JavaSocketConnector.isReady which   is updated by the java applet to notify that it was loaded.
    * [fix] Making socket listener to be started at the end of the
      socket creation to avoid firing onmessage before onopen.

--- socket-connector/JavaSocketConnector.js
+++ socket-connector/JavaSocketConnector.js
@@ -43,6 +43,12 @@ function JavaSocketConnector (params) {
 }
 
 /**
+ * @brief Global variable used to signal that the applet was loaded
+ * and started.
+ */
+JavaSocketConnector.isReady = false;
+
+/**
  * @brief Allows to send content over the provided socket object.
  *
  * @param content The content to be sent.
@@ -115,50 +121,3 @@ JavaSocketConnector.prototype.onlog = function (type, message) {
 };
 
 
-// Applet reports it is ready to use
-function java_socket_bridge_ready(){
-	java_socket_bridge_ready_flag = true;
-}
-
-// Connect to a given url and port
-function socket_connect(url, port){
-	if(java_socket_bridge_ready_flag){
-		return get_java_socket_bridge().connect(url, port);
-	}
-	else{
-		on_socket_error("Java Socket Bridge cannot connect until the applet has loaded");
-	}
-}
-
-// Disconnect
-function socket_disconnect(){
-	if(java_socket_bridge_ready_flag){
-		return get_java_socket_bridge().disconnect();
-	}
-	else{
-		on_socket_error("Java Socket Bridge cannot disconnect until the applet has loaded");
-	}
-}
-
-// Write something to the socket
-function socket_send(message){
-	if(java_socket_bridge_ready_flag){
-		return get_java_socket_bridge().send(message);
-	}
-	else{
-		on_socket_error("Java Socket Bridge cannot send a message until the applet has loaded");
-	}
-}
-
-// Get something from the socket
-function on_socket_get(message){}
-
-// Report an error
-function on_socket_error(message){
-	alert(message);
-}
-
-// Get the applet object
-function get_java_socket_bridge(){
-	return document.getElementById('JavaSocketConnector');
-}
