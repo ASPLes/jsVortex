b65c582c909c5fe26bd372ed68c542d8ccdd39b1
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2010-10-13 06:22:36 +0000

    jsVortex: * [fix] Initial modifications to use dojo shrinksafe to produce   a jsVortex release that is loadable from a single small file.

--- trunk/jsVortex/make-zip.sh
+++ trunk/jsVortex/make-zip.sh
@@ -4,16 +4,17 @@
 jsvortex_version=`cat VERSION`
 
 # create temporal link to simulate release directory
-ln -s src jsvortex-${jsvortex_version}
-ln -s ../doc/COPYING.txt src/COPYING.txt
-ln -s ../README src/README
-ln -s ../socket-connector/JavaSocketConnector.jar src/
-ln -s ../socket-connector/JavaSocketConnector.js src/
-zip jsVortex-${jsvortex_version}.zip \
-    jsvortex-${jsvortex_version}/*.{js,txt,jar} \
-    jsvortex-${jsvortex_version}/README
-rm src/README
-rm src/COPYING.txt
-rm src/JavaSocketConnector.jar
-rm src/JavaSocketConnector.js
-rm jsvortex-${jsvortex_version}
+DEST=jsvortex-${jsvortex_version}
+mkdir ${DEST}
+cp doc/COPYING.txt ${DEST}/COPYING.txt
+cp README ${DEST}/README
+cp socket-connector/JavaSocketConnector.jar ${DEST}/
+cp socket-connector/JavaSocketConnector.js ${DEST}/
+
+# prepare shrink-safe
+shrinksafe src/Vortex.js > ${DEST}/Vortex.js
+echo "Vortex.singleFile = true;"
+
+zip jsVortex-${jsvortex_version}.zip ${DEST}/*.{js,txt,jar} ${DEST}/README
+
+rm -rf jsvortex-${jsvortex_version}
