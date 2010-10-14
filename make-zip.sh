81439b090566505dab122b20195c4d0b14c7250e
Francis Brosnan Blázquez <francis@aspl.es>
2010-10-14 06:20:29 +0000

    jsVortex: * [fix] Updated make-zip.sh to use shrink safe to produce a jsVortex   bundle into a single file. Also used shrink safe to reduce   JavaSocketConnector.js

--- make-zip.sh
+++ make-zip.sh
@@ -4,17 +4,29 @@
 jsvortex_version=`cat VERSION`
 
 # create temporal link to simulate release directory
-DEST=jsvortex-${jsvortex_version}
+DEST=jsVortex-${jsvortex_version}
 mkdir ${DEST}
 cp doc/COPYING.txt ${DEST}/COPYING.txt
 cp README ${DEST}/README
 cp socket-connector/JavaSocketConnector.jar ${DEST}/
-cp socket-connector/JavaSocketConnector.js ${DEST}/
+shrinksafe socket-connector/JavaSocketConnector.js > ${DEST}/JavaSocketConnector.js
 
-# prepare shrink-safe
-shrinksafe src/Vortex.js > ${DEST}/Vortex.js
-echo "Vortex.singleFile = true;"
+# prepare shrink-safe: do initial file changing loading style
+shrinksafe src/Vortex.js | sed 's/singleFile:false/singleFile:true/g' > ${DEST}/Vortex.js
+
+# now append rest of files
+shrinksafe src/VortexConnection.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexEngine.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexChannel.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexTCPTransport.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexConnection_comp.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexXMLEngine.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexSASLEngine.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexFrame.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexSASLEnginePlain.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexSASLEngineAnonymous.js >> ${DEST}/Vortex.js
+shrinksafe src/VortexMimeHeader.js >> ${DEST}/Vortex.js
 
 zip jsVortex-${jsvortex_version}.zip ${DEST}/*.{js,txt,jar} ${DEST}/README
 
-rm -rf jsvortex-${jsvortex_version}
+rm -rf ${DEST}
