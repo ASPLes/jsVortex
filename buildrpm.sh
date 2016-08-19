067a1f375f1700a369d6393e7fb1582b02e7aa70
Francis Brosnan Blázquez <francis@aspl.es>
2016-08-19 17:24:22 +0200

    jsVortex: * [fix] Minor updates for Centos7 support..

--- buildrpm.sh
+++ buildrpm.sh
@@ -6,6 +6,7 @@ echo "Preparing sources"
 rm -rf rpm/SOURCES/*.tar.gz
 rm -rf rpm/BUILD/*
 rm -rf rpm/BUILDROOT/*
+test -d rpm/RPMS || mkdir -p rpm/RPMS
 find rpm/RPMS/ -type f -exec rm {} \;
 # rm *.zip
 # make
@@ -13,6 +14,14 @@ find rpm/RPMS/ -type f -exec rm {} \;
 
 echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/jsVortex/rpm' rpm/SPECS/jsvortex.spec
+error=$?
+if [ $error != 0 ]; then
+    echo "ERROR: ***"
+    echo "ERROR: rpmbuild command failed, exitcode=$error"
+    echo "ERROR: ***"
+    exit $error
+fi
+
 
 echo "Output ready at rpm/RPMS"
 find rpm/RPMS -type f -name '*.rpm' > rpm/RPMS/files
