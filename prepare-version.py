14d111db6611f92ad436c568258ee17d8be4a258
Francis Brosnan Blázquez <francis.brosnan@aspl.es>
2024-02-23 12:56:22 +0000

    jsVortex: * [fix] Updated VERSION and prepare-version.py to conform github.com which   how has disabled svn

--- prepare-version.py
+++ prepare-version.py
@@ -4,18 +4,29 @@ from core_admin_common import command
 import sys
 
 # update sources
-(status, info) = command.run ("svn update .")
-if status:
-    print "ERROR: failed to update svn, error was: %s" % info
-    sys.exit (-1)
+# 23/02/2024 -- | github.com disabled svn
+# 23/02/2024 -- (status, info) = command.run ("svn update .")
+cmds = [
+    "git fetch",
+    "git pull --ff-only"
+]
+for cmd in cmds:
+    # run commands to update local code
+    (status, info) = command.run (cmd)
+    if status:
+        print "ERROR: failed to update svn, error was: %s" % info
+        sys.exit (-1)
+    # end if
+# end for
 
 # update version file
-(status, info) = command.run ("LANG=C svn info . | grep Revision")
+(status, info) = command.run ("""LANG=C git log | grep "^commit " | wc -l""")
 if status:
     print "ERROR: failed to svn revision, error was: %s" % info
     sys.exit (-1)
 
-revision = info.strip ().split (": ")[1]
+# revision = info.strip ().split (": ")[1]
+revision = info.strip ()
 
 version = open ("VERSION").read ().strip ().split (".g")[0]
 
